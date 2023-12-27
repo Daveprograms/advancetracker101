00d9111d1e891ce895d640292b976ab17beebfb6
David Fashola <davidprograms7@gmsil.com>
2023-12-27 13:03:04 -0500

    tracker

---
+++ track.py
@@ -0,0 +1,36 @@
+import phonenumbers
+import opencage
+import folium 
+
+from number import number
+
+from phonenumbers import geocoder
+pepnumber = phonenumbers.parse(number)
+location = geocoder.description_for_number(pepnumber, "en")
+print(location)
+
+
+from phonenumbers import carrier
+service_pro = phonenumbers.parse(number)
+print( carrier.name_for_number(service_pro, "en"))
+
+from opencage.geocoder import OpenCageGeocode
+
+key ='0e8a15fc81a443b7ade66ae577a3e441'
+
+geocoder = OpenCageGeocode(key)
+query = str(location)
+results = geocoder.geocode(query)
+
+print(results)
+
+
+lat = results[0]['geometry']['lat']
+lng = results[0]['geometry']['lng']
+
+print(lat,lng)
+
+myMap = folium.Map(location =[lat, lng], zoom_start=9)
+folium.Marker([lat,lng], popup=location).add_to(myMap)
+
+myMap.save("mylocation.html")
